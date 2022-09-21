3d9bc5d9af56f1cd6494652c9958d8bc6c2b154d
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-21 23:52:50 +0300

    Changed keybindings for nvim-tree

--- lua/user/pluginconf/tree-conf.lua
+++ lua/user/pluginconf/tree-conf.lua
@@ -4,7 +4,7 @@ if not ok then return end
 vim.api.nvim_set_keymap("n", "<leader>t", ":NvimTreeToggle<CR>", { silent = true, noremap = true })
 
 tree.setup({
-   hijack_cursor = false,
+   hijack_cursor = true,
    hijack_unnamed_buffer_when_opening = false,
    sync_root_with_cwd = true,
 
@@ -20,23 +20,24 @@ tree.setup({
       mappings = {
          custom_only = true,
          list = {
-            { key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-            { key = "<C-e>", action = "edit_in_place" },
-            { key = "O", action = "edit_no_picker" },
-            { key = { "\\", "<2-RightMouse>" }, action = "cd" },
+            { key = "<cr>", action = "toggle_node" },
+            { key = "i", action = "edit" },
+            { key = "<C-i>", action = "edit_in_place" },
+            -- { key = "O", action = "edit_no_picker" },
+            { key = { "\\", "l" }, action = "cd" },
             { key = "<C-v>", action = "vsplit" },
             { key = "<C-s>", action = "split" },
-            { key = "<C-t>", action = "tabnew" },
+            -- { key = "<C-t>", action = "tabnew" },
             { key = "<", action = "prev_sibling" },
             { key = ">", action = "next_sibling" },
             { key = "P", action = "parent_node" },
-            { key = "<BS>", action = "close_node" },
-            { key = "<Tab>",  action = "preview" },
+            { key = "<bs>", action = "close_node" },
+            { key = "<tab>",  action = "preview" },
             { key = "K", action = "first_sibling" },
             { key = "J", action = "last_sibling" },
             { key = "I", action = "toggle_git_ignored" },
             { key = "H", action = "toggle_dotfiles" },
-            { key = "U", action = "toggle_custom" },
+            -- { key = "U", action = "toggle_custom" },
             { key = "R", action = "refresh" },
             { key = "a", action = "create" },
             { key = "d", action = "remove" },
@@ -49,19 +50,24 @@ tree.setup({
             { key = "Y", action = "copy_path" },
             { key = "gy", action = "copy_absolute_path" },
             { key = "[d", action = "prev_diag_item" },
-            { key = "[g", action = "prev_git_item" },
             { key = "]d", action = "next_diag_item" },
+            { key = "[g", action = "prev_git_item" },
             { key = "]g", action = "next_git_item" },
-            { key = "-", action = "dir_up" },
+            { key = { "h", "-" } , action = "dir_up" },
             -- { key = "s", action = "system_open" },
-            { key = "f", action = "live_filter" },
-            { key = "F", action = "clear_live_filter" },
-            { key = "q", action = "close" },
+
+            -- NOTE: filters are really bad.
+            --       if its not gonna get an update which fixes the bugs
+            --       and makes them look better I will just keep using the default
+            --       buffer search
+            -- { key = "f", action = "live_filter" },
+            -- { key = "F", action = "clear_live_filter" },
+            { key = { "q", "<esc>" }, action = "close" },
             { key = "E", action = "collapse_all" },
             { key = "e", action = "expand_all" },
             -- { key = "S", action = "search_node" },
             { key = ".", action = "run_file_command" },
-            { key = "<C-k>", action = "toggle_file_info" },
+            { key = "K", action = "toggle_file_info" },
             { key = "?", action = "toggle_help" },
             { key = "mm", action = "toggle_mark" },
             { key = "mv", action = "bulk_move" },
