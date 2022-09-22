af8ecfbd9e1a83a27056aa6df3a93455a414d019
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-22 23:05:09 +0300

    Changed the config of the lightspeed plugin

--- lua/user/pluginconf/lightspeed-conf.lua
+++ lua/user/pluginconf/lightspeed-conf.lua
@@ -2,19 +2,12 @@ local ok, lightspeed = pcall(require, "lightspeed")
 if not ok then return end
 
 lightspeed.setup({
-   ignore_case = false,
+   ignore_case = true,
    exit_after_idle_msecs = { unlabeled = nil, labeled = nil },
    --- s/x ---
-   jump_to_unique_chars = { safety_timeout = 400 },
+   jump_to_unique_chars = false,
    match_only_the_start_of_same_char_seqs = true,
-   force_beacons_into_match_width = false,
-   -- Display characters in a custom way in the highlighted matches.
-   -- substitute_chars = { ['\n'] = '¬', },
-   -- Leaving the appropriate list empty effectively disables "smart" mode,
-   -- and forces auto-jump to be on or off.
-   -- safe_labels = { . . . },
-   -- labels = { . . . },
-   -- These keys are captured directly by the plugin at runtime.
+   force_beacons_into_match_width = true,
    special_keys = {
       next_match_group = '<tab>',
       prev_match_group = '<s-tab>',
