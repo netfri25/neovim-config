44b047765cfceca8cfb421bc3d9307b358178bad
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-22 22:20:33 +0300

    Fixed broken config for trevj

--- lua/user/pluginconf/trevj-conf.lua
+++ lua/user/pluginconf/trevj-conf.lua
@@ -2,8 +2,8 @@ local ok, trevj = pcall(require, "trevj")
 if not ok then return end
 
 local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
+local keymap = vim.keymap.set
 
-keymap('n', '<leader>j', trevj.format_at_cursor, opts)
+keymap('n', '<leader>j', function() require('trevj').format_at_cursor() end, opts)
 
 trevj.setup({})
