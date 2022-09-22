87172a80db77ed78d5babaa9c30e638fab71c24d
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-22 21:22:33 +0300

    Changed config for the whichkey plugin

--- lua/user/pluginconf/whichkey-conf.lua
+++ lua/user/pluginconf/whichkey-conf.lua
@@ -15,4 +15,11 @@ whichkey.setup({
       border = "rounded",
       margin = { 2, 2, 2, 1 }
    },
+
+   layout = {
+      height = { min = 4, max = 20 },
+      width = { min = 20, max = 50 },
+      spacing = 3,
+      align = "center",
+   }
 })
