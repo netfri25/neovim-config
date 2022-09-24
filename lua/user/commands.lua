c7d233411fb96a143a83285800b6b9d98e51ece3
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-24 23:08:44 +0300

    Switched from a giant vim.cmd to vim.api functions, and removed useless user commands

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -1,51 +1,21 @@
-vim.cmd([[
-   let @/=""
+vim.api.nvim_create_autocmd('Filetype', {
+   pattern = 'haskell',
+   command = 'setlocal shiftwidth=2, softtabstop=2',
+})
 
-   " custom files
-   au BufRead,BufNewFile *.jva set filetype=java
-   au BufRead,BufNewFile *.ktn set filetype=kitten
+vim.api.nvim_create_autocmd('Filetype', {
+   pattern = 'json',
+   command = 'syntax match Comment +\\/\\/.\\+$+',
+})
 
-   " custom file settings
-   autocmd Filetype haskell setlocal shiftwidth=2 softtabstop=2
-   autocmd FileType json syntax match Comment +\/\/.\+$+
+vim.api.nvim_create_user_command('Reload', function()
+   local path = vim.fn.stdpath('config') .. '/init.lua'
+   vim.api.nvim_command('source ' .. path)
+end, {})
 
-   " custom commands
-   function! OpenConfig()
-      edit /home/netfri/.config/nvim/init.lua
-   endfunction
-   command! Config call OpenConfig()
-
-   function! OpenPlugins()
-      edit /home/netfri/.config/nvim/lua/plugins.lua
-   endfunction
-   command! Plugins call OpenPlugins()
-
-   function OpenLspConfig()
-      edit /home/netfri/.config/nvim/lua/pluginconf/lsp-conf.lua
-   endfunction
-   command! LspConfig call OpenLspConfig()
-
-   function OpenNVimDir()
-      edit /home/netfri/.config/nvim
-   endfunction
-   command! NVDir call OpenNVimDir()
-
-   function ReloadConfig()
-      source /home/netfri/.config/nvim/init.lua
-   endfunction
-   command! Reload call ReloadConfig()
-
-   " save and load folds
-   " augroup remember_folds
-   "   autocmd!
-   "   au BufWinLeave ?* silent! mkview 1
-   "   au BufWinEnter ?* silent! loadview 1
-   " augroup END
-
-   " set viewoptions-=options
-
-   augroup highlight_yank
-       autocmd!
-       au TextYankPost * silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 }
-   augroup END
-]])
+vim.api.nvim_create_autocmd('TextYankPost', {
+   pattern = '*',
+   callback = function()
+      vim.api.nvim_command([[ silent! lua vim.highlight.on_yank { higroup='IncSearch', timeout=200 } ]])
+   end
+})
