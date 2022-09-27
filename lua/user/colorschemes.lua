7911e17623b1a6745c0374036482b5a2cbccdbae
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-27 22:34:26 +0300

    Added the ability to decide if yo change the cursor line number

--- lua/user/colorschemes.lua
+++ lua/user/colorschemes.lua
@@ -1,23 +1,25 @@
 vim.g.material_style = 'darker' -- Gruvbox material style
 vim.g.onedark_config = { style = 'warm' }
 
-local function apply_colorscheme(name)
+local function apply_colorscheme(name, change_cursorline)
    vim.api.nvim_command('colorscheme ' .. name)
    vim.api.nvim_set_hl(0, 'CursorLine', {})
-   vim.api.nvim_set_hl(0, 'CursorLineNr', { link = 'Special' })
+   if change_cursorline == true then
+      vim.api.nvim_set_hl(0, 'CursorLineNr', { link = 'Special' })
+   end
 end
 
-local function func_colorscheme(name)
+local function func_colorscheme(name, change_cursorline)
    return function()
-      apply_colorscheme(name)
+      apply_colorscheme(name, change_cursorline)
    end
 end
 
 -- for easy switching between colorschemes and applying the cursor highlighters
-vim.api.nvim_create_user_command('Gruvbox', func_colorscheme('gruvbox-material'), {})
-vim.api.nvim_create_user_command('VSCode', func_colorscheme('vscode'), {})
-vim.api.nvim_create_user_command('Onedark', func_colorscheme('onedark'), {})
-vim.api.nvim_create_user_command('Sublime', func_colorscheme('ofirkai'), {})
+vim.api.nvim_create_user_command('Gruvbox', func_colorscheme('gruvbox-material', true), {})
+vim.api.nvim_create_user_command('VSCode', func_colorscheme('vscode', true), {})
+vim.api.nvim_create_user_command('Onedark', func_colorscheme('onedark', true), {})
+vim.api.nvim_create_user_command('Sublime', func_colorscheme('ofirkai', false), {})
 
 -- might add something in the future that will remember the last colorscheme I used
 local colorscheme_cmd = 'Gruvbox'
