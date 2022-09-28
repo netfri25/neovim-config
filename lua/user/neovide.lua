2fc9180140849d283b0e248f59fd07bc690bfb37
Netanel Friedman <netanel.friedman6@gmail.com>
2022-09-28 14:10:23 +0300

    Changed Neovide's refresh rate to 300 and some other minor changes

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,16 +1,17 @@
-vim.g.neovide_cursor_vfx_mode = 'ripple'
-vim.g.neovide_refresh_rate = 150
+-- vim.g.neovide_cursor_vfx_mode = 'ripple'
+vim.g.neovide_refresh_rate = 300
 vim.g.neovide_fullscreen = true
 vim.g.neovide_input_use_logo = true
-vim.g.neovide_no_idle = true
--- vim.g.neovide_refresh_rate_idle = 5
+vim.g.neovide_no_idle = false
+vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_scroll_animation_length = 0.7
 vim.g.neovide_underline_automatic_scaling = true
 vim.g.neovide_cursor_trail_length = 0.01
 vim.g.neovide_cursor_animation_length = 0.07
 vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
+-- vim.g.neovide_profiler = true
 
-vim.g.gui_font_default_size = 12
+vim.g.gui_font_default_size = 10
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = 'DejaVuSansMono Nerd Font'
 
@@ -32,5 +33,5 @@ end
 ResetGuiFont()
 
 -- Keymaps
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-=>', function() ResizeGuiFont(1) end, { silent = true, desc = 'Decrease GUI font size' })
-vim.keymap.set({ 'n', 'i', 'v' }, '<C-->', function() ResizeGuiFont(-1) end, { silent = true, desc = 'Increase GUI font size' })
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-=>', function() ResizeGuiFont(2) end, { silent = true, desc = 'Decrease GUI font size' })
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-->', function() ResizeGuiFont(-2) end, { silent = true, desc = 'Increase GUI font size' })
