5a33eaebead1de156e0c67438e43d5daf495fcdc
Netanel Friedman <netanel.friedman6@gmail.com>
2022-10-01 20:50:26 +0300

    Remove useless autocmd

--- lua/user/pluginconf/cmdheight-conf.lua
+++ lua/user/pluginconf/cmdheight-conf.lua
@@ -9,8 +9,3 @@ vim.api.nvim_create_autocmd('CmdlineLeave', {
    command = 'set cmdheight=0',
    group = group,
 })
-
-vim.api.nvim_create_autocmd('CmdlineChanged', {
-   command = 'redraw',
-   group = group,
-})
