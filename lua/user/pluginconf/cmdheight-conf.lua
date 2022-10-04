e03aa59fb5cb580f0542477800b38941cce7ea56
Netanel <netfri@user.lan>
2022-10-04 17:02:40 +0300

    Add more things that go into the cmdline

--- lua/user/pluginconf/cmdheight-conf.lua
+++ lua/user/pluginconf/cmdheight-conf.lua
@@ -2,8 +2,11 @@
 
 vim.keymap.set('c', '<cr>', '<cr><cmd>set cmdheight=0<cr>', { silent = true })
 vim.keymap.set('c', '<esc>', '<cr><cmd>set cmdheight=0<cr>', { silent = true })
+vim.keymap.set('c', '<c-c>', '<cr><cmd>set cmdheight=0<cr>', { silent = true })
 vim.keymap.set({ 'n', 'v', 'x' }, ':', '<cmd>set cmdheight=2<cr>:', { silent = true })
 vim.keymap.set({ 'n', 'v', 'x' }, ';', '<cmd>set cmdheight=2<cr>:', { silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '/', '<cmd>set cmdheight=2<cr>/', { silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '?', '<cmd>set cmdheight=2<cr>?', { silent = true })
 
 -- vim.api.nvim_create_autocmd('CmdlineEnter', {
 --    command = 'set cmdheight=2',
