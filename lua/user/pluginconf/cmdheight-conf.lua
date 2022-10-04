ea297163d1edaa1cefd0e9a1b25bd911920eae1b
Netanel <netfri@user.lan>
2022-10-04 16:57:51 +0300

    Remove the need for autocmd to change the cmdheight

--- lua/user/pluginconf/cmdheight-conf.lua
+++ lua/user/pluginconf/cmdheight-conf.lua
@@ -1,11 +1,16 @@
-local group = vim.api.nvim_create_augroup('CmdheightChanger', { clear = true })
+-- local group = vim.api.nvim_create_augroup('CmdheightChanger', { clear = true })
 
-vim.api.nvim_create_autocmd('CmdlineEnter', {
-   command = 'set cmdheight=2',
-   group = group,
-})
+vim.keymap.set('c', '<cr>', '<cr><cmd>set cmdheight=0<cr>', { silent = true })
+vim.keymap.set('c', '<esc>', '<cr><cmd>set cmdheight=0<cr>', { silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, ':', '<cmd>set cmdheight=2<cr>:', { silent = true })
+vim.keymap.set({ 'n', 'v', 'x' }, ';', '<cmd>set cmdheight=2<cr>:', { silent = true })
 
-vim.api.nvim_create_autocmd('CmdlineLeave', {
-   command = 'set cmdheight=0',
-   group = group,
-})
+-- vim.api.nvim_create_autocmd('CmdlineEnter', {
+--    command = 'set cmdheight=2',
+--    group = group,
+-- })
+--
+-- vim.api.nvim_create_autocmd('CmdlineLeave', {
+--    command = 'set cmdheight=0',
+--    group = group,
+-- })
