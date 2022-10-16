f712a3daafe888ae50c6a1b4318ff681f2ee9f89
Netanel Friedman <netanel.friedman6@gmail.com>
2022-10-16 11:38:00 +0300

    Add filepath to completion options

--- lua/user/pluginconf/cmp-conf.lua
+++ lua/user/pluginconf/cmp-conf.lua
@@ -25,6 +25,7 @@ cmp.setup({
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
       { name = 'nvim_lsp_signature_help' },
+      { name = 'path' },
    }, {
       { name = 'buffer' }
    }),
