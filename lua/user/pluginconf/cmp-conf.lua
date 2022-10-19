0b67445330afcb74d988ca43f5265ef1cfb17583
Netanel Friedman <netanel.friedman6@gmail.com>
2022-10-19 08:33:11 +0300

    Remove a comment

--- lua/user/pluginconf/cmp-conf.lua
+++ lua/user/pluginconf/cmp-conf.lua
@@ -18,7 +18,7 @@ cmp.setup({
       ['<C-f>'] = cmp.mapping.scroll_docs(4),
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
-      ['<Tab>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item.
+      ['<Tab>'] = cmp.mapping.confirm({ select = true }),
    }),
 
    sources = cmp.config.sources({
