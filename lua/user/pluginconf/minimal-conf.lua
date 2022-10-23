5f3565f67c3cbd518dcc07042638a24a9762c39d
Netanel Friedman <netanel.friedman6@gmail.com>
2022-10-23 20:04:50 +0300

    Remove minimal-conf

--- lua/user/pluginconf/minimal-conf.lua
+++
@@ -1,7 +0,0 @@
-vim.g.minimal_italic_comments = true
-vim.g.minimal_italic_keywords = false
-vim.g.minimal_italic_booleans = false
-vim.g.minimal_italic_functions = false
-vim.g.minimal_italic_variables = false
-vim.g.minimal_transparent_background = false
-
