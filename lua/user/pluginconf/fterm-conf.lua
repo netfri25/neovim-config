a6e0717bcbf3d1c4c5be74c75941c3d4cfadf2fd
Netanel Friedman <netanel.friedman6@gmail.com>
2022-11-18 12:38:53 +0200

    Move fterm a bit upwards

--- lua/user/pluginconf/fterm-conf.lua
+++ lua/user/pluginconf/fterm-conf.lua
@@ -8,4 +8,11 @@ fterm.setup({
    border = 'rounded',
    cmd = '/usr/bin/fish',
    auto_close = true,
+
+   dimensions = {
+      width = 0.8,
+      height = 0.8,
+      x = 0.5,
+      y = 0.32,
+   },
 })
