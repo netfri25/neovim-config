b444dc8e47a2ef3c6a49519b3d2abe98b80cb79b
Netanel Friedman <netanel.friedman6@gmail.com>
2022-12-17 18:31:15 +0200

    Specify the needed parsers for treesitter instead of just installing all of them

--- lua/user/pluginconf/treesitter-conf.lua
+++ lua/user/pluginconf/treesitter-conf.lua
@@ -2,7 +2,21 @@ local ok, tree = pcall(require, 'nvim-treesitter.configs')
 if not ok then return end
 
 tree.setup({
-   ensure_installed = 'all',
+   ensure_installed = {
+      'c',
+      'cpp',
+      'vim',
+      'lua',
+      'rust',
+      'java',
+      'toml',
+      'json',
+      'ocaml',
+      'python',
+      'haskell',
+      'markdown',
+   },
+
    sync_install = false,
    auto_install = true,
    ignore_install = { 'phpdoc' },
