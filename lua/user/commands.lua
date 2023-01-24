dd99484c20d72973039ab82a8a03d25e600c096e
Netanel Friedman <netanel.friedman6@gmail.com>
2023-01-24 21:33:44 +0200

    Add augroup for highlighted yanked text

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -1,19 +1,23 @@
 vim.api.nvim_create_autocmd('Filetype', {
+   desc = 'Specific filetype indentation',
    pattern = 'haskell',
    command = 'setlocal shiftwidth=2 | setlocal softtabstop=2 | setlocal expandtab',
 })
 
 vim.api.nvim_create_autocmd('Filetype', {
+   desc = 'Specific filetype indentation',
    pattern = 'json',
    command = 'syntax match Comment +\\/\\/.\\+$+',
 })
 
 vim.api.nvim_create_autocmd('Filetype', {
+   desc = 'Specific filetype indentation',
    pattern = 'rust',
    command = 'setlocal shiftwidth=4 | setlocal tabstop=4',
 })
 
 vim.api.nvim_create_autocmd('Filetype', {
+   desc = 'Adds key mappings for the `help` buffer',
    pattern = 'help',
    callback = function()
       local buf = vim.api.nvim_get_current_buf()
@@ -33,6 +37,7 @@ vim.api.nvim_create_autocmd('Filetype', {
 })
 
 vim.api.nvim_create_autocmd('TermLeave,TermClose', {
+   desc = 'Resets the scroll offset that the terminal buffer changes',
    callback = function()
       vim.o.scrolloff = 100
    end
@@ -42,10 +47,14 @@ vim.api.nvim_create_autocmd('TermLeave,TermClose', {
 vim.api.nvim_create_user_command('Reload', function()
    local path = vim.fn.stdpath('config') .. '/init.lua'
    vim.api.nvim_command('source ' .. path)
-end, {})
+end, { desc = 'Reloads the user configuration' })
+
+local hi_yank_group = vim.api.nvim_create_augroup('HighlightYank', { clear = true })
 
 -- highlighting yanked text regions
 vim.api.nvim_create_autocmd('TextYankPost', {
+   desc = 'Highlights the yanked text on yank',
+   group = hi_yank_group,
    pattern = '*',
    callback = function()
       vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 200 });
