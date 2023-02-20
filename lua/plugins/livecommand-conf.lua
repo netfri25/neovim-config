594bb5f658a24a8490ac8ad169bd6b086093ebde
Netanel Friedman <netanel.friedman6@gmail.com>
2023-02-20 21:16:18 +0200

    Fix live-command plugin

--- lua/plugins/livecommand-conf.lua
+++ lua/plugins/livecommand-conf.lua
@@ -1,9 +1,11 @@
 return {
    'smjonas/live-command.nvim',
    lazy = false,
-   opts = {
-      commands = {
-         Norm = { cmd = 'norm' },
-      },
-   }
+   config = function()
+         require('live-command').setup({
+         commands = {
+            Norm = { cmd = 'norm' },
+         },
+      })
+   end
 }
