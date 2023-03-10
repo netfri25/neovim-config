a52ddb3d2b9435a90db607ceaa341a71f66aefe1
Netanel Friedman <netanel.friedman6@gmail.com>
2023-03-10 16:42:24 +0200

    Reduce useless code

--- lua/user/colorschemes.lua
+++ lua/user/colorschemes.lua
@@ -1,32 +1,12 @@
-vim.g.material_style = 'darker' -- Gruvbox material style
-
-local function apply_colorscheme(name, change_cursorline)
-   vim.api.nvim_command('colorscheme ' .. name)
-   vim.api.nvim_set_hl(0, 'CursorLine', {})
-   vim.api.nvim_set_hl(0, 'FloatBorder', {})
-
-   if change_cursorline then
-      vim.api.nvim_set_hl(0, 'CursorLineNr', { link = 'Special' })
-   end
-end
-
-local function create_colorscheme_command(command_name, name, change_cursorline)
-   local function func()
-      apply_colorscheme(name, change_cursorline)
-   end
-
-   vim.api.nvim_create_user_command(command_name, func, {})
-end
-
 vim.api.nvim_create_autocmd('ColorScheme', {
    callback = function()
       vim.api.nvim_set_hl(0, 'CursorLine', {})
+      vim.api.nvim_set_hl(0, 'FloatBorder', {})
+      vim.api.nvim_set_hl(0, 'CursorLineNr', { link = 'Special' })
    end
 })
 
-create_colorscheme_command('MelioraT', 'mellifluous', true)
-
 -- might add something in the future that will remember the last colorscheme I used
-local default_colorscheme = 'MelioraT'
+local default_colorscheme = 'mellifluous'
 
-vim.api.nvim_command(default_colorscheme)
+vim.api.nvim_command('color ' .. default_colorscheme)
