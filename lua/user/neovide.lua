370ef8dab74b9778a3800dc5ae017b6518f22e9f
Netanel Friedman <netanel.friedman6@gmail.com>
2023-04-01 22:24:21 +0300

    Set FPS to 120 in neovide

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,5 +1,5 @@
 -- vim.g.neovide_cursor_vfx_mode = 'ripple'
--- vim.g.neovide_refresh_rate = 120
+vim.g.neovide_refresh_rate = 120
 vim.g.neovide_fullscreen = true
 vim.g.neovide_input_use_logo = true
 vim.g.neovide_no_idle = false
@@ -10,7 +10,7 @@ vim.g.neovide_underline_automatic_scaling = true
 vim.g.neovide_cursor_trail_size = 0.6
 vim.g.neovide_cursor_animation_length = 0.07
 vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
--- vim.g.neovide_profiler = true
+vim.g.neovide_profiler = false
 
 vim.g.gui_font_default_size = 15
 vim.g.gui_font_size = vim.g.gui_font_default_size
