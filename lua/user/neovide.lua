2cc70ca0d1450826c72023bc3f24dc0b4c4f4b2b
Netanel Friedman <netanel.friedman6@gmail.com>
2023-04-08 21:45:42 +0300

    Change some neovide animations settings

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,14 +1,13 @@
 -- vim.g.neovide_cursor_vfx_mode = 'ripple'
 vim.g.neovide_refresh_rate = 120
 vim.g.neovide_fullscreen = true
-vim.g.neovide_input_use_logo = true
 vim.g.neovide_no_idle = false
 vim.g.neovide_refresh_rate_idle = 5
-vim.g.neovide_scroll_animation_length = 0.4
+vim.g.neovide_scroll_animation_length = 0.5
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_underline_automatic_scaling = true
-vim.g.neovide_cursor_trail_size = 0.6
-vim.g.neovide_cursor_animation_length = 0.07
+vim.g.neovide_cursor_trail_size = 0.7
+vim.g.neovide_cursor_animation_length = 0.08
 vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
 vim.g.neovide_profiler = false
 
