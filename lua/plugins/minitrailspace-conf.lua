02bd5f059f00c4eefbc0c4bcab730f8a4b95da9c
Netanel Friedman <netanel.friedman6@gmail.com>
2023-05-30 22:47:17 +0300

    Replace the vim-whitespace plugin with mini.trailspace

---
+++ lua/plugins/minitrailspace-conf.lua
@@ -0,0 +1,23 @@
+local enabled = true
+
+local function switch(fun1, fun2)
+   enabled = not enabled
+   if enabled then
+      return fun1
+   else
+      return fun2
+   end
+end
+
+return {
+   'echasnovski/mini.trailspace',
+   lazy = false,
+
+   config = function()
+      local trailspace = require('mini.trailspace')
+      trailspace.setup()
+      vim.api.nvim_create_user_command("TrimWhitespace", trailspace.trim, { desc = "Trim whitespace" })
+      vim.keymap.set('n', '<leader>w', function() switch(trailspace.highlight, trailspace.unhighlight)() end,
+         { silent = true, desc = 'Toggle trailing whitespace' })
+   end,
+}
