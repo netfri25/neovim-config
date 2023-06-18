e194752a0ba66dce37d2388b62b0a85949f3e541
Netanel Friedman <netanel.friedman6@gmail.com>
2023-06-18 13:49:42 +0300

    Set the contrast for the mellifluous colorscheme

--- lua/plugins/mellifluous-conf.lua
+++ lua/plugins/mellifluous-conf.lua
@@ -10,20 +10,24 @@ return {
       dim_inactive = false,
       neutral = true, -- set this to `true` for neutral background and greys.
 
+      mellifluous = {
+         bg_contrast = 'medium',
+      },
+
       styles = {
-         comments = 'italic',
-         conditionals = 'NONE',
-         folds = 'NONE',
-         loops = 'NONE',
-         functions = 'NONE',
-         keywords = 'NONE',
-         strings = 'NONE',
-         variables = 'NONE',
-         numbers = 'NONE',
-         booleans = 'NONE',
-         properties = 'NONE',
-         types = 'NONE',
-         operators = 'NONE',
+         comments = { italic = true },
+         conditionals = {},
+         folds = {},
+         loops = {},
+         functions = {},
+         keywords = {},
+         strings = {},
+         variables = {},
+         numbers = {},
+         booleans = {},
+         properties = {},
+         types = {},
+         operators = {},
       },
 
       transparent_background = {
@@ -38,16 +42,16 @@ return {
       plugins = {
          cmp = true,
          indent_blankline = false,
-         startify = true,
+         startify = false,
 
          nvim_tree = {
-            enabled = true,
-            show_root = true,
+            enabled = false,
+            show_root = false,
          },
 
          telescope = {
             enabled = false,
-            nvchad_like = true,
+            nvchad_like = false,
          },
       },
    }
