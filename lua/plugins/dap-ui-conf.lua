6185b1d4a457c430b3fcf60450c240a9a8163029
Netanel Friedman <netanel.friedman6@gmail.com>
2023-07-14 17:02:13 +0300

    Add the dap plugin for debugging programs (will ve a lot of help in the future)

---
+++ lua/plugins/dap-ui-conf.lua
@@ -0,0 +1,28 @@
+return {
+   'rcarriga/nvim-dap-ui',
+   dependencies = { 'mfussenegger/nvim-dap' },
+   lazy = true,
+   event = 'VeryLazy',
+   ft = { 'c', 'cpp', 'rust' },
+
+   keys = {
+      { '<leader>do', function() require('dapui').toggle() end, desc = "Toggle dap-ui" },
+      { '<leader>da', function() require('dapui').eval() end, desc = "dap eval" },
+   },
+
+   config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+         dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+         dapui.close()
+      end
+
+      dapui.setup({
+         floating = {
+            border = 'rounded',
+         }
+      })
+   end
+}
