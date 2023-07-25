16d56f790659dd468503060d04b00b0bdbbe69eb
Netanel Friedman <netanel.friedman6@gmail.com>
2023-07-25 20:51:46 +0300

    Make the battery status update every 5 minutes

--- lua/plugins/battery-conf.lua
+++ lua/plugins/battery-conf.lua
@@ -9,7 +9,7 @@ return {
    },
 
    opts = {
-      update_rate_seconds = 30, -- Number of seconds between checking battery status
+      update_rate_seconds = 5 * 60, -- Number of seconds between checking battery status
       show_status_when_no_battery = true, -- Don't show any icon or text when no battery found (desktop for example)
       show_plugged_icon = true, -- If true show a cable icon alongside the battery icon when plugged in
       show_unplugged_icon = false, -- When true show a diconnected cable icon when not plugged in
