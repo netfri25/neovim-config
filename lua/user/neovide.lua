b4999052bfa73106b85f27c6445e06c9fed82555
Netanel Friedman <netanel.friedman6@gmail.com>
2023-08-23 07:43:02 +0300

    Use fullscreen by default in neovide

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,6 +1,6 @@
 -- vim.g.neovide_cursor_vfx_mode = 'ripple'
 vim.g.neovide_refresh_rate = 60
-vim.g.neovide_fullscreen = false
+vim.g.neovide_fullscreen = true
 vim.g.neovide_no_idle = false
 vim.g.neovide_refresh_rate_idle = 5
 vim.g.neovide_scroll_animation_length = 0.5
