33e854e8af299576070c9bd207907e235efa38e0
Netanel Friedman <netanel.friedman6@gmail.com>
2023-09-01 12:41:53 +0300

    Add neogit

--- lua/plugins/gruvbox-conf.lua
+++ lua/plugins/gruvbox-conf.lua
@@ -2,57 +2,68 @@ return {
    'ellisonleao/gruvbox.nvim',
    lazy = false,
 
-   opts = {
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-         strings = false,
-         comments = true,
-         operators = false,
-         folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      invert_intend_guides = false,
-      inverse = true, -- invert background for search, diffs, statuslines and errors
-      contrast = 'hard', -- can be 'hard', 'soft' or empty string
-      palette_overrides = {},
+   config = function()
+      local contrast = 'hard' -- can be 'hard', 'soft' or empty string
+      local colors = require("gruvbox.palette").get_base_colors(vim.o.background, contrast)
+      require('gruvbox').setup({
+         undercurl = true,
+         underline = true,
+         bold = true,
+         italic = {
+            strings = false,
+            comments = true,
+            operators = false,
+            folds = true,
+         },
+         strikethrough = true,
+         invert_selection = false,
+         invert_signs = false,
+         invert_tabline = false,
+         invert_intend_guides = false,
+         inverse = true, -- invert background for search, diffs, statuslines and errors
+         contrast = contrast,
+         palette_overrides = {},
 
-      overrides = {
-         SignColumn = { link = 'Background' },
-         LspInlayHint = { fg = '#666666', italic = true },
-         MatchParen = { link = 'GruvboxOrange' },
-         NormalFloat = { link = 'Normal' },
+         overrides = {
+            SignColumn = { link = 'Background' },
+            LspInlayHint = { fg = '#666666', italic = true },
+            MatchParen = { link = 'GruvboxOrange' },
+            NormalFloat = { link = 'Normal' },
 
-         GruvboxRedUnderline = { underline = true, undercurl = false },
-         GruvboxGreenUnderline = { underline = true, undercurl = false },
-         GruvboxYellowUnderline = { underline = true, undercurl = false },
-         GruvboxBlueUnderline = { underline = true, undercurl = false },
-         GruvboxPurpleUnderline = { underline = true, undercurl = false },
-         GruvboxAquaUnderline = { underline = true, undercurl = false },
-         GruvboxOrangeUnderline = { underline = true, undercurl = false },
+            GruvboxRedUnderline = { underline = true, undercurl = false },
+            GruvboxGreenUnderline = { underline = true, undercurl = false },
+            GruvboxYellowUnderline = { underline = true, undercurl = false },
+            GruvboxBlueUnderline = { underline = true, undercurl = false },
+            GruvboxPurpleUnderline = { underline = true, undercurl = false },
+            GruvboxAquaUnderline = { underline = true, undercurl = false },
+            GruvboxOrangeUnderline = { underline = true, undercurl = false },
 
-         Pmenu = { link = 'Background' },
-         PmenuSel = { link = 'Background' },
-         PmenuSbar = { link = 'Background' },
-         PmenuThumb = { link = 'Background' },
+            Pmenu = { link = 'Background' },
+            PmenuSel = { link = 'Background' },
+            PmenuSbar = { link = 'Background' },
+            PmenuThumb = { link = 'Background' },
 
-         Operator = { link = 'GruvboxFg4' }, -- operators
+            GitSignsAdd = { fg = colors.green, bg = colors.bg0 },
+            GitSignsChange = { fg = colors.aqua, bg = colors.bg0 },
+            GitSignsDelete = { fg = colors.red, bg = colors.bg0 },
 
-         ['@variable'] = { link = 'GruvboxFg2' }, -- variables
-         ['@field'] = { link = '@variable' }, -- variables
-         ['@parameter'] = { link = '@variable' }, -- variables
-         -- ['@constructor'] = { link = 'Operator' }, -- operators
-         ['@punctuation.delimiter'] = { link = 'Operator' }, -- operators
-         ['@punctuation.bracket'] = { link = 'Operator' }, -- operators
-         ['@punctuation.special'] = { link = 'Operator' }, -- operators
-         ['@type.qualifier'] = { link = '@variable.builtin' },
-      },
+            DiffAdd = { link = 'GruvboxGreenSign' },
+            DiffDelete = { link = 'GruvboxRedSign' },
 
-      dim_inactive = false,
-      transparent_mode = false,
-   }
+            Operator = { link = 'GruvboxFg4' }, -- operators
+
+            ['@variable'] = { link = 'GruvboxFg2' }, -- variables
+            ['@field'] = { link = '@variable' }, -- variables
+            ['@parameter'] = { link = '@variable' }, -- variables
+            -- ['@constructor'] = { link = 'Operator' }, -- operators
+            ['@punctuation.delimiter'] = { link = 'Operator' }, -- operators
+            ['@punctuation.bracket'] = { link = 'Operator' }, -- operators
+            ['@punctuation.special'] = { link = 'Operator' }, -- operators
+            ['@type.qualifier'] = { link = '@variable.builtin' },
+         },
+
+         dim_inactive = false,
+         transparent_mode = false,
+      })
+   end
 }
