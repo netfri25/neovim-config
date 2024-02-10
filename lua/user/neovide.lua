2b9ad5878f31c6f44802ba8effc0618368753771
Netanel Friedman <netanel.friedman6@gmail.com>
2024-02-10 22:16:19 +0200

    Modify some of neovide options

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -3,19 +3,20 @@ vim.g.neovide_refresh_rate = 60
 vim.g.neovide_fullscreen = true
 vim.g.neovide_no_idle = false
 vim.g.neovide_refresh_rate_idle = 5
-vim.g.neovide_scroll_animation_length = 0.5
+vim.g.neovide_scroll_animation_length = 0.17
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_underline_automatic_scaling = true
-vim.g.neovide_cursor_trail_size = 0.7
+vim.g.neovide_cursor_trail_size = 0.8
 vim.g.neovide_cursor_animation_length = 0.08
 vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
 vim.g.neovide_profiler = false
+vim.g.neovide_remember_window_size = true
 
 vim.g.gui_font_default_size = 11
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = 'Iosevka Custom'
 vim.g.gui_antialiasing = 'antialias'
-vim.g.gui_hinting = 'none'
+vim.g.gui_hinting = 'normal'
 
 RefreshGuiFont = function()
    vim.opt.guifont = string.format(
