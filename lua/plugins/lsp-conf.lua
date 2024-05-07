b6ca5509660a91c7194bef566feeac6ee4643c8b
Netanel Friedman <netanel.friedman6@gmail.com>
2024-05-07 23:50:30 +0300

    Use qflist for diagnostics instead of loclist

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -45,7 +45,7 @@ return {
          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { silent = true, desc = 'Show diagnostics' })
          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { silent = true, desc = 'Show previous diagnostics' })
          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { silent = true, desc = 'Show next diagnostics' })
-         vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { silent = true, desc = 'Show loclist' })
+         vim.keymap.set('n', '<leader>q', vim.diagnostic.setqflist, { silent = true, desc = 'Show loclist' })
          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { silent = true, desc = 'Goto declaration' })
          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { silent = true, desc = 'Goto definition' })
          vim.keymap.set('n', 'K', vim.lsp.buf.hover, { silent = true, desc = 'Show docs' })
