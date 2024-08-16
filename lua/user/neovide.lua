73410f2a9aeea829046d7036fca971b0abfc5dbb
Netanel Friedman <netanel.friedman6@gmail.com>
2024-08-16 18:57:47 +0300

    Changes to neovide settings

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,16 +1,16 @@
 -- vim.g.neovide_cursor_vfx_mode = 'ripple'
-vim.g.neovide_refresh_rate = 60
+-- vim.g.neovide_refresh_rate = 240
 vim.g.neovide_fullscreen = true
-vim.g.neovide_no_idle = false
-vim.g.neovide_refresh_rate_idle = 5
-vim.g.neovide_scroll_animation_length = 0.1
+-- vim.g.neovide_no_idle = true
+-- vim.g.neovide_refresh_rate_idle = 5
+vim.g.neovide_scroll_animation_length = 0.15
 vim.g.neovide_hide_mouse_when_typing = true
-vim.g.neovide_underline_automatic_scaling = true
+-- vim.g.neovide_underline_automatic_scaling = true
 vim.g.neovide_cursor_trail_size = 0.8
 vim.g.neovide_cursor_animation_length = 0.06
 vim.g.neovide_cursor_vfx_particle_lifetime = 0.8
-vim.g.neovide_profiler = false
-vim.g.neovide_remember_window_size = false
+-- vim.g.neovide_profiler = false
+-- vim.g.neovide_remember_window_size = false
 
 vim.g.gui_font_default_size = 12
 vim.g.gui_font_size = vim.g.gui_font_default_size
