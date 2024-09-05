f008a5e9c730c526ca507ac8d1694c44659b84f4
Netanel Friedman <netanel.friedman6@gmail.com>
2024-09-05 12:54:53 +0300

    Don't start neovide in fullscreen

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -4,7 +4,7 @@ if not vim.g['neovide'] then return end
 -- vim.g.neovide_refresh_rate = 240
 -- vim.g.neovide_no_idle = true
 -- vim.g.neovide_refresh_rate_idle = 5
-vim.g.neovide_fullscreen = true
+vim.g.neovide_fullscreen = false
 vim.g.neovide_scroll_animation_length = 0.15
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_underline_automatic_scaling = false
