636f4cda6c242c3132d4cc9d0fbf8af2831aee2b
Netanel Friedman <netanel.friedman6@gmail.com>
2024-11-11 20:03:18 +0200

    Make the suda write variable non-lazy

--- lua/plugins/suda-conf.lua
+++ lua/plugins/suda-conf.lua
@@ -1,11 +1,8 @@
 -- another gigantic configuration file with a lot of effort put into it
 -- lets me write files using sudo
+vim.g.suda_smart_edit = 1
 return {
    'lambdalisue/suda.vim',
    lazy = true,
    cmd = { 'SudaRead', 'SudaWrite' },
-
-   config = function()
-      vim.g.suda_smart_edit = 1
-   end
 }
