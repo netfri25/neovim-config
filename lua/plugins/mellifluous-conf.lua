96cf869b4426d3d65871263cc625f44ac3cac06f
Netanel Friedman <netanel.friedman6@gmail.com>
2024-11-11 19:32:31 +0200

    Remove the condig for the mellifluous colorscheme

--- lua/plugins/mellifluous-conf.lua
+++ lua/plugins/mellifluous-conf.lua
@@ -1,60 +1,12 @@
 -- Colorscheme
 
 return {
-   'ramojus/mellifluous.nvim',
-   lazy = false,
+    'ramojus/mellifluous.nvim',
+    lazy = false,
 
-   dependencies = {
-      'rktjmp/lush.nvim'
-   },
+    dependencies = {
+        'rktjmp/lush.nvim'
+    },
 
-   opts = {
-      dim_inactive = false,
-      neutral = true, -- set this to `true` for neutral background and greys.
-
-      mellifluous = {
-         bg_contrast = 'medium',
-      },
-
-      styles = {
-         comments = { italic = true },
-         conditionals = {},
-         folds = {},
-         loops = {},
-         functions = {},
-         keywords = {},
-         strings = {},
-         variables = {},
-         numbers = {},
-         booleans = {},
-         properties = {},
-         types = {},
-         operators = {},
-      },
-
-      transparent_background = {
-         enabled = false,
-         floating_windows = false,
-         telescope = true,
-         file_tree = true,
-         cursor_line = true,
-         status_line = false,
-      },
-
-      plugins = {
-         cmp = true,
-         indent_blankline = false,
-         startify = false,
-
-         nvim_tree = {
-            enabled = false,
-            show_root = false,
-         },
-
-         telescope = {
-            enabled = false,
-            nvchad_like = false,
-         },
-      },
-   }
+    config = true,
 }
