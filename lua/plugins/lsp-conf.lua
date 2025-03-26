43cec58576eb03e4a9a9070957b8bd134d99a672
Netanel Friedman <netanel.friedman6@gmail.com>
2025-03-26 22:37:14 +0200

    Sometimes I hate using neovim (btw)

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -35,26 +35,19 @@ return {
             },
         })
 
-        vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
-        vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
-
-
         local function on_attach(client, bufnr)
             -- client.server_capabilities.semanticTokensProvider = nil
-            -- no need for that
-            -- navbuddy.attach(client, bufnr)
-            require('lsp-inlayhints').on_attach(client, bufnr)
             vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { silent = true, desc = 'Show diagnostics' })
-            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { silent = true, desc = 'Show previous diagnostics' })
-            vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { silent = true, desc = 'Show next diagnostics' })
+            vim.keymap.set('n', '[d', function() vim.diagnostic.jump({count=-1, float=true}) end, { silent = true, desc = 'Show previous diagnostics' })
+            vim.keymap.set('n', ']d', function() vim.diagnostic.jump({count= 1, float=true}) end, { silent = true, desc = 'Show next diagnostics' })
             vim.keymap.set('n', '<leader>q', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.ERROR }) end, { silent = true, desc = 'Show loclist' })
-            vim.keymap.set('n', '<leader>Q', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.WARNING }) end, { silent = true, desc = 'Show loclist' })
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { silent = true, desc = 'Goto declaration' })
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { silent = true, desc = 'Goto definition' })
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, { silent = true, desc = 'Show docs' })
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { silent = true, desc = 'Goto implementation' })
-            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, { silent = true, desc = 'Show signature help' })
-            vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { silent = true, desc = 'Show type definition' })
+            vim.keymap.set('n', '<leader>Q', function() vim.diagnostic.setqflist({ severity = vim.diagnostic.severity.WARN }) end, { silent = true, desc = 'Show loclist' })
+            vim.keymap.set('n', 'gD', function() vim.lsp.buf.declaration({ border = "rounded" }) end, { silent = true, desc = 'Goto declaration' })
+            vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition({ border = "rounded" }) end, { silent = true, desc = 'Goto definition' })
+            vim.keymap.set('n', 'K', function() vim.lsp.buf.hover({ border = "rounded" }) end, { silent = true, desc = 'Show docs' })
+            vim.keymap.set('n', 'gi', function() vim.lsp.buf.implementation({ border = "rounded" }) end, { silent = true, desc = 'Goto implementation' })
+            vim.keymap.set('n', '<C-k>', function() vim.lsp.buf.signature_help({ border = "rounded" }) end, { silent = true, desc = 'Show signature help' })
+            vim.keymap.set('n', 'gt', function() vim.lsp.buf.type_definition({ border = "rounded" }) end, { silent = true, desc = 'Show type definition' })
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { silent = true, desc = 'Rename symbol' })
             vim.keymap.set('n', '<leader>ca', function() require('tiny-code-action').code_action() end, { silent = true, desc = 'Code actions' })
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, { silent = true, desc = 'Show references' })
