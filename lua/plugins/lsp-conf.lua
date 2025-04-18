5b53d93932a8fb9177e4948ecca1fced206b5a20
Netanel Friedman <netanel.friedman6@gmail.com>
2025-04-19 01:20:24 +0300

    Update lua-ls config

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -219,20 +219,39 @@ return {
         lspconfig['lua_ls'].setup({
             capabilities = capabilities,
             on_attach = on_attach,
+            on_init = function(client)
+                if client.workspace_folders then
+                    local path = client.workspace_folders[1].name
+                    if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc')) then
+                        return
+                    end
+                end
+
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                    runtime = {
+                        -- Tell the language server which version of Lua you're using
+                        -- (most likely LuaJIT in the case of Neovim)
+                        version = 'LuaJIT'
+                    },
+                    -- Make the server aware of Neovim runtime files
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                            -- Depending on the usage, you might want to add additional paths here.
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                })
+            end,
 
             settings = {
                 Lua = {
                     completion = {
                         callSnippet = 'Replace',
-                    },
-
-                    diagnostics = {
-                        globals = { 'vim' },
-                    },
-
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file('', true),
-                        checkThirdParty = false,
                     },
 
                     runtime = { version = 'LuaJIT' },
