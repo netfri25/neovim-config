5db58c973f02b89adc00984feb4eec5dcc38cea7
Netanel Friedman <netanel.friedman6@gmail.com>
2025-04-19 00:40:37 +0300

    Make dap use gdb

--- lua/plugins/dap-conf.lua
+++ lua/plugins/dap-conf.lua
@@ -17,19 +17,16 @@ return {
     config = function()
         local dap = require('dap')
 
-        dap.adapters.codelldb = {
-            type = 'server',
-            port = '${port}',
-            executable = {
-                command = vim.fn.exepath('codelldb'),
-                args = { '--port', '${port}' }
-            }
+        dap.adapters.gdb = {
+            type = "executable",
+            command = "gdb",
+            args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
         }
 
         dap.configurations.c = {
             {
                 name = "Launch file",
-                type = "codelldb",
+                type = "gdb",
                 request = "launch",
                 program = function() return vim.fn.input('Path to executable: ') end,
                 cwd = '${workspaceFolder}',
