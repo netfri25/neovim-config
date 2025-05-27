28237299bc299dd3c4b32907c2bbd258a407df09
Netanel Friedman <netanel.friedman6@gmail.com>
2025-05-27 17:27:54 +0300

    Change neovide config

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -7,6 +7,8 @@ vim.g.neovide_no_idle = false
 vim.g.neovide_fullscreen = false
 vim.g.neovide_scroll_animation_length = 0.2
 vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_cursor_trail_size = 0.8
+vim.g.neovide_cursor_antialiasing = false
 -- vim.g.neovide_underline_automatic_scaling = false
 -- vim.g.neovide_cursor_trail_size = 0.7
 -- vim.g.neovide_cursor_animation_length = 0.07
@@ -17,8 +19,8 @@ vim.g.neovide_hide_mouse_when_typing = true
 vim.g.gui_font_default_size = 14
 vim.g.gui_font_size = vim.g.gui_font_default_size
 vim.g.gui_font_face = 'IosevkaCustomNerdFontComplete Nerd Font'
-vim.g.gui_antialiasing = 'antialias'
-vim.g.gui_hinting = 'normal'
+vim.g.gui_antialiasing = 'subpixelantialias'
+vim.g.gui_hinting = 'full'
 
 vim.keymap.set({ 'n' }, '<F11>', function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end, { desc = "Toggle fullscreen in neovide" })
 
@@ -48,3 +50,6 @@ ResetGuiFont()
 -- Keymaps
 vim.keymap.set({ 'n', 'i', 'v' }, '<C-=>', function() ResizeGuiFont(1) end, { silent = true, desc = 'Decrease GUI font size' })
 vim.keymap.set({ 'n', 'i', 'v' }, '<C-->', function() ResizeGuiFont(-1) end, { silent = true, desc = 'Increase GUI font size' })
+
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s-=>', function() ResizeGuiFont(1) end, { silent = true, desc = 'Decrease GUI font size' })
+vim.keymap.set({ 'n', 'i', 'v' }, '<C-s-->', function() ResizeGuiFont(-1) end, { silent = true, desc = 'Increase GUI font size' })
